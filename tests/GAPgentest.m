c22b181bfebe89928b06b773e4557440563946aa
nikcleju <none@none>
2011-11-10 13:33:37 +0000

    --HG-- extra : convert_revision : svn%3A053afec8-576b-4fb6-bc25-02a7d36b1f22/trunk%4032

--- tests/GAPgentest.m
+++ tests/GAPgentest.m
@@ -1,40 +1,38 @@
 % Run GAP and save parameters and solutions as reference test data
 % to check if other algorithms are correct
 
-numA = 10;
-numY = 100;
+% numA = 10;
+% numY = 100;
+% 
+% sizesA{1} = [50 100];
+% sizesA{2} = [20 25];
+% sizesA{3} = [10 120];
+% sizesA{4} = [15 100];
+% sizesA{5} = [70 100];
+% sizesA{6} = [80 100];
+% sizesA{7} = [90 100];
+% sizesA{8} = [99 100];
+% sizesA{9} = [100 100];
+% sizesA{10} = [250 400];
+% for i = 1:numA sizesA{i} = fliplr(sizesA{i}); end
+% 
+% for i = 1:numA
+%     sz = sizesA{i};
+%     cellA{i} = randn(sz);
+%     m = round((0.2 + 0.6*rand)*sz(2));
+%     cellM{i} = randn(m,sz(2));
+%     cellY{i} = randn(m, numY);
+%     cellXinit{i} = zeros(sz(2), numY);
+%     for j = 1:numY
+%         cellEps{i}(j) = rand / 100; % restrict from 0 to 1% of measurements
+%     end
+% end
+% opt_num_iteration = 1000;
+% opt_greedy_level = 0.9;
+% opt_stopping_coefficient_size = 1e-4;
+% opt_l2solver = 'pseudoinverse';
 
-sizesA{1} = [50 100];
-sizesA{2} = [20 25];
-sizesA{3} = [10 120];
-sizesA{4} = [15 100];
-sizesA{5} = [70 100];
-sizesA{6} = [80 100];
-sizesA{7} = [90 100];
-sizesA{8} = [99 100];
-sizesA{9} = [100 100];
-sizesA{10} = [250 400];
-for i = 1:numA sizesA{i} = fliplr(sizesA{i}); end
-
-sigmamin = [0.00001 0.01 0.2 0.3 0.4 0.0001 0.1 0.001 0.1 0.1];
-
-for i = 1:numA
-    sz = sizesA{i};
-    cellA{i} = randn(sz);
-    m = round((0.2 + 0.6*rand)*sz(2));
-    cellM{i} = randn(m,sz(2));
-    cellY{i} = randn(m, numY);
-    cellXinit{i} = zeros(sz(2), numY);
-    for j = 1:numY
-        cellEps{i}(j) = rand / 100; % restrict from 0 to 1% of measurements
-    end
-end
-opt_num_iteration = 1000;
-opt_greedy_level = 0.9;
-opt_stopping_coefficient_size = 1e-4;
-opt_l2solver = 'pseudoinverse';
-
-%load GAPtestdata
+load GAPtestdata
 tic
 for i = 1:numA
     for j = 1:numY
@@ -49,4 +47,4 @@ for i = 1:numA
 end
 toc
 
-save GAPtestdata
+% save GAPtestdata
