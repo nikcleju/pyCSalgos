1b306bef25e896b8a450da1c66b2a501c63262c1
nikcleju <none@none>
2012-02-24 18:30:42 +0000

    Added setup.py and README.txt
    --HG--
    extra : convert_revision : svn%3A053afec8-576b-4fb6-bc25-02a7d36b1f22/trunk%4095

---
+++ README.txt
@@ -0,0 +1,10 @@
+Nicolae Cleju
+Technical University Gheorghe Asachi of Iasi, Romania
+Queen Mary, University of London
+24.02.2012
+
+Python Compressed Sensing algorithms
+-------------------------------------
+
+Python implementation of various Compressed Sensing algorithms originally implemented in Matlab.
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+    name = "pyCSalgos",
+    packages = ["pyCSalgos","pyCSalgos/BP","pyCSalgos/GAP","pyCSalgos/NESTA","pyCSalgos/OMP","pyCSalgos/RecomTST","pyCSalgos/SL0"],
+    version = "1.0.0",
+    description = "Python Compressed Sensing algorithms",
+    author = "Nicolae Cleju",
+    author_email = "nikcleju@gmail.com",
+    utl = 'https://code.soundsoftware.ac.uk/projects/svsa',
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics"
+        ],
+    long_description = """\
+Python Compressed Sensing algorithms
+-------------------------------------
+
+Python implementation of various Compressed Sensing algorithms originally implemented in Matlab.
+
+"""
+)
