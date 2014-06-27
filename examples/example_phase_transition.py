d76d753afa166ba82be60499b872b5c5c87bef93
Nic Cleju <nikcleju@gmail.com>
2014-06-27 16:05:34 +0300

    Added AnalysisPhaseTransition, SynthesisPhaseTransition and abstract base class PhaseTransition. Added example.

--- examples/example_phase_transition.py
+++ examples/example_phase_transition.py
@@ -5,24 +5,49 @@ An example of generating a phase transition plot
 """
 
 import numpy
-from pyCSalgos.phase_transition import PhaseTransition
+from pyCSalgos.phase_transition import SynthesisPhaseTransition, AnalysisPhaseTransition
 from pyCSalgos.omp import OrthogonalMatchingPursuit
 from pyCSalgos.l1min import L1Min
 from pyCSalgos.sl0 import SmoothedL0
 from pyCSalgos.tst import TwoStageThresholding
+from pyCSalgos.analysisl1min import AnalysisL1Min
+from pyCSalgos.analysis_by_synthesis import AnalysisBySynthesis
 
-def run():
-    n, N = 50, 70
+
+def run_synthesis():
+    signal_size, dict_size = 50, 70
     deltas = numpy.arange(0.1, 0.9, 0.1)
     rhos = numpy.arange(0.1, 0.9, 0.1)
-    pt = PhaseTransition(n, N, deltas, rhos, 3,
-                         [OrthogonalMatchingPursuit(1e-6, algorithm="sparsify_QR"),
-                          L1Min(1e-6),
-                          SmoothedL0(1e-6),
-                          TwoStageThresholding(1e-6)])
+
+    print "Running synthesis phase transition..."
+    pt = SynthesisPhaseTransition(signal_size, dict_size, deltas, rhos, 3,
+                                  [OrthogonalMatchingPursuit(1e-6, algorithm="sparsify_QR"),
+                                   L1Min(1e-6),
+                                   SmoothedL0(1e-6),
+                                   TwoStageThresholding(1e-6)])
     pt.run()
     pt.plot()
+
     print "Example finished."
 
+
+def run_analysis():
+    signal_size, dict_size = 50, 70
+    deltas = numpy.arange(0.1, 0.9, 0.1)
+    rhos = numpy.arange(0.1, 0.9, 0.1)
+
+    print "Running analysis phase transition..."
+    pt = AnalysisPhaseTransition(signal_size, dict_size, deltas, rhos, 3,
+                                 [AnalysisL1Min(1e-6),
+                                  AnalysisBySynthesis(L1Min(1e-6)),
+                                  AnalysisBySynthesis(OrthogonalMatchingPursuit(1e-6, algorithm="sparsify_QR")),
+                                  AnalysisBySynthesis(SmoothedL0(1e-6))])
+    pt.run()
+    pt.plot()
+
+    print "Example finished."
+
+
 if __name__ == "__main__":
-    run()
+    run_synthesis()
+    run_analysis()
--- pyCSalgos/src/phase_transition.py
+++ pyCSalgos/src/phase_transition.py
@@ -8,6 +8,9 @@ Class for fast generation of phase-transition graphs
 # Author: Nicolae Cleju
 # License: BSD 3 clause
 
+from six import with_metaclass
+from abc import ABCMeta, abstractmethod
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -16,16 +19,69 @@ import matplotlib.colors as mcolors
 import generate as gen
 
 
-class PhaseTransition:
-
-    def __init__(self, signaldim, dictdim, deltas, rhos, Ndata, solvers):
+class PhaseTransition(with_metaclass(ABCMeta, object)):
+    def __init__(self, signaldim, dictdim, deltas, rhos, numdata, solvers):
         self.signaldim = signaldim
         self.dictdim = dictdim
-        self.Ndata = Ndata
+        self.numdata = numdata
         self.deltas = deltas
         self.rhos = rhos
         self.solvers = solvers
         self.avgerr = None
+
+    @abstractmethod
+    def run(self):
+        """
+        Runs
+        """
+
+    def plot(self, subplot=True):
+        if self.avgerr is None:
+            ValueError("No data to plot (have you run()?)")
+
+        if subplot is False:
+            pass
+        elif subplot is True:
+            numsolvers = len(self.avgerr)
+            if numsolvers == 1:
+                subplot = False
+            if numsolvers == 2:
+                subplotlayout = (1,2)
+            elif numsolvers == 3:
+                subplotlayout = (1,3)
+            elif numsolvers == 4:
+                subplotlayout = (2,2)
+            elif numsolvers == 5:
+                subplotlayout = (2,3)
+            elif numsolvers == 6:
+                subplotlayout = (2,3)
+        elif len(subplot) == 2:
+            subplotlayout = tuple(i for i in subplot)
+        else:
+            raise ValueError("Incorrect 'subplot' parameter")
+
+        for i, solver in enumerate(self.avgerr.keys()):
+            if subplot is True:
+                plt.subplot(*(subplotlayout+(i,)))
+            else:
+                plt.figure()
+            plot_phase_transition(self.avgerr[solver])
+            plt.title(solver)
+            plt.xlabel(r"$\delta$")
+            plt.ylabel(r"$\rho$")
+
+        plt.show()
+
+    #TODO: save()
+
+
+class SynthesisPhaseTransition(PhaseTransition):
+    """
+    Class for running and plotting synthesis-based phase transitions
+    """
+
+    def __init__(self, signaldim, dictdim, deltas, rhos, numdata, solvers):
+        super(SynthesisPhaseTransition, self).__init__(signaldim, dictdim, deltas, rhos, numdata, solvers)
 
     def run(self):
         self.avgerr = dict()
@@ -39,7 +95,7 @@ class PhaseTransition:
 
                 measurements, acqumatrix, realdata, dictionary, realgamma, realsupport = \
                     gen.make_compressed_sensing_problem(
-                        m, self.signaldim, self.dictdim, k, self.Ndata, "randn", "randn")
+                        m, self.signaldim, self.dictdim, k, self.numdata, "randn", "randn")
 
                 for solver in self.solvers:
                     gamma = solver.solve(measurements, np.dot(acqumatrix, dictionary))
@@ -50,37 +106,38 @@ class PhaseTransition:
                         errors[:, i] = errors[:, i] / np.linalg.norm(realdata[:, i])
                     self.avgerr[solver][idelta, irho] = np.mean(np.sqrt(sum(errors**2, 0)))
 
-    def plot(self, subplot=True):
-        if self.avgerr is None:
-            ValueError("No data to plot (have you run()?)")
 
-        if subplot == True:
-            numsolvers = len(self.avgerr)
-            if numsolvers == 2:
-                subplotlayout = (1,2)
-            elif numsolvers == 3:
-                subplotlayout = (1,3)
-            elif numsolvers == 4:
-                subplotlayout = (2,2)
-            elif numsolvers == 5:
-                subplotlayout = (2,3)
-            elif numsolvers == 6:
-                subplotlayout = (2,3)
-        elif len(subplot) == 2:
-            subplotlayout = (i for i in subplot)
-        else:
-            raise ValueError("Incorrect 'subplot' parameter")
+# TODO: maybe needs refactoring, it's very similar to PhaseTransition()
+class AnalysisPhaseTransition(PhaseTransition):
+    """
+    Class for running and plotting analysis-based phase transitions
+    """
 
-        for i, solver in enumerate(self.avgerr.keys()):
-            plt.subplot(*(subplotlayout+(i,)))
-            plot_phase_transition(self.avgerr[solver])
-            plt.title(solver)
-            plt.xlabel(r"$\delta$")
-            plt.ylabel(r"$\rho$")
+    def __init__(self, signaldim, operatordim, deltas, rhos, numdata, solvers):
+        super(AnalysisPhaseTransition, self).__init__(signaldim, operatordim, deltas, rhos, numdata, solvers)
 
-        plt.show()
+    def run(self):
+        self.avgerr = dict()
+        for solver in self.solvers:
+            self.avgerr[solver] = np.zeros(shape=(len(self.deltas), len(self.rhos)))
 
-    #TODO: save()
+        for idelta, delta in enumerate(self.deltas):
+            for irho, rho in enumerate(self.rhos):
+                m = int(round(self.signaldim * delta, 0))  # delta = m/n
+                l = self.signaldim - int(round(m * rho, 0))             # rho = (n-l)/m
+
+                #TODO: make operator only once
+                measurements, acqumatrix, realdata, operator, realgamma, realcosupport = \
+                    gen.make_analysis_compressed_sensing_problem(
+                        m, self.signaldim, self.dictdim, l, self.numdata, "randn", "randn")
+
+                for solver in self.solvers:
+                    data = solver.solve(measurements, acqumatrix, operator)
+
+                    errors = data - realdata
+                    for i in range(errors.shape[1]):
+                        errors[:, i] = errors[:, i] / np.linalg.norm(realdata[:, i])
+                    self.avgerr[solver][idelta, irho] = np.mean(np.sqrt(sum(errors**2, 0)))
 
 
 # TODO: add many more parameters
@@ -100,3 +157,4 @@ def plot_phase_transition(matrix):
     # plt.figure()
     # Transpose the data so first axis = horizontal, use inverse colormap so small(good) = white, origin = lower left
     plt.imshow(bigmatrix.T, cmap=cm.gray_r, norm=mcolors.Normalize(0, 1), interpolation='nearest', origin='lower')
+
