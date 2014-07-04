f979a449eefcd64beeef039fc541dd09aa22efc3
Nic Cleju <nikcleju@gmail.com>
2014-07-04 16:17:04 +0300

    Added everything in package root via __init__.py

---
+++ pyCSalgos/__init__.py
@@ -0,0 +1,37 @@
+"""
+The :mod:`pyCSalgos` module: Python Compressed Sensing algorithms.
+"""
+
+from .generate import make_sparse_coded_signal
+from .generate import make_compressed_sensing_problem
+from .generate import make_cosparse_coded_signal
+from .generate import make_analysis_compressed_sensing_problem
+
+from .omp import OrthogonalMatchingPursuit
+from .l1min import L1Min
+from .sl0 import SmoothedL0
+from .tst import TwoStageThresholding
+
+from .analysisl1min import AnalysisL1Min
+from .gap import GreedyAnalysisPursuit
+from .analysis_by_synthesis import AnalysisBySynthesis
+from .uap import UnconstrainedAnalysisPursuit
+
+from .phase_transition import SynthesisPhaseTransition
+from .phase_transition import AnalysisPhaseTransition
+
+
+__all__ = ['make_sparse_coded_signal',
+           'make_compressed_sensing_problem',
+           'make_cosparse_coded_signal',
+           'make_analysis_compressed_sensing_problem',
+           'OrthogonalMatchingPursuit',
+           'L1Min',
+           'SmoothedL0',
+           'TwoStageThresholding',
+           'AnalysisL1Min',
+           'GreedyAnalysisPursuit',
+           'AnalysisBySynthesis',
+           'UnconstrainedAnalysisPursuit',
+           'SynthesisPhaseTransition',
+           'AnalysisPhaseTransition']
