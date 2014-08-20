866dbacd2441a95acadf7ee7f8d2cbf846ed230c
Nic Cleju <nikcleju@gmail.com>
2014-08-20 20:53:29 +0300

    Added AMP and AIHT to example phase transition

--- examples/example_phase_transition.py
+++ examples/example_phase_transition.py
@@ -15,19 +15,40 @@ from pyCSalgos import AnalysisL1Min
 from pyCSalgos import AnalysisBySynthesis
 from pyCSalgos import GreedyAnalysisPursuit
 from pyCSalgos import UnconstrainedAnalysisPursuit
+from pyCSalgos import ApproximateMessagePassing
+from pyCSalgos import IterativeHardThresholding
 
-
-def run_synthesis():
+def run_test():
     signal_size, dict_size = 50, 70
-    deltas = numpy.arange(0.1, 0.9, 0.1)
-    rhos = numpy.arange(0.1, 0.9, 0.1)
+    deltas = numpy.arange(0.1, 1, 0.1)
+    rhos = numpy.arange(0.1, 1, 0.2)
 
     print "Running synthesis phase transition..."
     pt = SynthesisPhaseTransition(signal_size, dict_size, deltas, rhos, 3,
                                   [OrthogonalMatchingPursuit(1e-6, algorithm="sparsify_QR"),
+                                    ])
+    pt.run()
+    pt.plot()
+
+    print "Example finished."
+
+def run_synthesis():
+    signal_size, dict_size = 50, 100
+    deltas = numpy.arange(0.1, 1, 0.03)
+    rhos = numpy.arange(0.1, 1, 0.03)
+    #signal_size, dict_size = 512,512
+    #deltas = [0.5]
+    #rhos = numpy.arange(0.1, 0.51, 0.01)
+
+
+    print "Running synthesis phase transition..."
+    pt = SynthesisPhaseTransition(signal_size, dict_size, deltas, rhos, 30,
+                                  [OrthogonalMatchingPursuit(1e-6, algorithm="sparsify_QR"),
                                    L1Min(1e-6),
                                    SmoothedL0(1e-6),
-                                   TwoStageThresholding(1e-6)])
+                                   TwoStageThresholding(1e-6),
+                                   ApproximateMessagePassing(1e-6, 1000),
+                                   IterativeHardThresholding(1e-10, sparsity="real", maxiter=1000) ])
     pt.run()
     pt.plot()
 
@@ -36,8 +57,8 @@ def run_synthesis():
 
 def run_analysis():
     signal_size, dict_size = 50, 70
-    deltas = numpy.arange(0.1, 0.9, 0.1)
-    rhos = numpy.arange(0.1, 0.9, 0.1)
+    deltas = numpy.arange(0.1, 1, 0.1)
+    rhos = numpy.arange(0.1, 1, 0.1)
 
     print "Running analysis phase transition..."
     pt = AnalysisPhaseTransition(signal_size, dict_size, deltas, rhos, 3,
@@ -66,6 +87,7 @@ def run_uap():
 
 
 if __name__ == "__main__":
-    #run_synthesis()
-    run_analysis()
+    run_synthesis()
+    #run_analysis()
     #run_uap()
+    #run_test()
